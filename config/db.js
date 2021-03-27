86544bb4312b38e0820c6d9446a093f2e608fd25
suryanto23 <sur23.id@gmail.com>
2021-03-27 22:02:28 +0700

    Adding promise config

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 mongoose.connect(MONGODB_LIVE , {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+}).then((value)=>{})
+
 
 const dbConfigMongo = mongoose.connection;
 module.exports = dbConfigMongo;
